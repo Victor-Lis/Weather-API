c4399cd34c368a8ec78e60edfda229db0e4ab0df
Victor-Lis <victorlisbronzo1@gmail.com>
2023-11-05 23:16:06 -0300

    v1

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 // Variaveis
 
 const apiKey = "9639efc9770bb4cb1d25209fea1642bc"
-const apiCountryURL = "https://countryflagsapi.com/png/"
+const apiCountryURL = "https://flagsapi.com/"
 
 const cityInput = document.querySelector("#city-input")
 const searchBtn = document.querySelector("#search")
@@ -18,45 +18,52 @@ const windElement = document.querySelector("#wind span")
 // Funções
 
 const getWeatherData = async (city) => {
-    
+
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
 
-    const res = await fetch(apiWeatherURL)
-    const data = await res.json();
+    const data = await fetch(apiWeatherURL)
+        .then(data => data.json())
 
-    console.log(data)
-
-    return data
+    if(data.cod == 200){
+        return data
+    }else{
+        alert(`Opa! Parece que não conseguimos encontrar :/`)
+        return false
+    }
 
 }
 
 const showWeatherData = async (city) => {
 
-    const data = await getWeatherData(city)
+    let data = await getWeatherData(city)
 
-    cityElement.innerText = data.name
-    tempElement.innerText = parseInt(data.main.temp)
-    descElement.innerText = data.weather[0].description
-    wheatherIconElement.setAttribute(
-        "src",
-        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-    )
-    countryElement.setAttribute("src", apiCountryURL + data.sys.country)
-    humidityElement.innerText = `${data.main.humidity}%`
-    windElement.innerText = `${data.wind.speed}km/h`
+    if (!!data) {
+        wheaterData.classList.remove("hide")
+        cityElement.innerText = data.name
+        tempElement.innerText = parseInt(data.main.temp)
+        descElement.innerText = data.weather[0].description
+        wheatherIconElement.setAttribute(
+            "src",
+            `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+        )
+        countryElement.setAttribute("src", `${apiCountryURL}${data.sys.country}/flat/64.png`)
+        humidityElement.innerText = `${data.main.humidity}%`
+        windElement.innerText = `${data.wind.speed}km/h`
+    }
 
 }
 
 // Eventos
 
 searchBtn.addEventListener("click", (e) => {
-    
+
     e.preventDefault()
 
     const city = cityInput.value
 
-    showWeatherData(city)
+    if (!!city) {
+        showWeatherData(city)
+    }
 
-    wheaterData.classList.remove("hide")
 
 })
